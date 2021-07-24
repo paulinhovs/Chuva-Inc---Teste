6a6edb19c3fab392ece6b38c748b3d79809c21ef
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2021-07-24 17:10:25 -0300

    Adição de conteudo e funcionalidade no resumo, espaço de discussões e responsividade nos conteudos (Alterações feitas em computador sem acesso ao git)

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
       dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
+      btnText.innerHTML = "ver mais";
+      moreText.style.display = "none"
+  } else {
       dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
+      btnText.innerHTML = "ver menos";
       moreText.style.display = "inline";
-    }
   }
+}
